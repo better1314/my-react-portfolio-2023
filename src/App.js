3d10a126af325ad81c53b3c6ec4f893519e4980a
Gan Zhi Kiat <ganzk1998@gmail.com>
2023-07-13 21:02:49 +0800

    added AboutMe page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './css/App.css';
 import Header from './Components/Header'
 import Mainpage from './Components/Mainpage'
 import Footer from './Components/Footer'
+import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Mainpage/>
-
+      <AboutMe/>
       <Footer/>
     </div>
   );
---
+++ src/Components/AboutMe.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../css/App.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook,faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+function Description(){
+    return(
+        <>
+        <div className='description-container'>
+            <div className="desctiption-center">
+                <div className="profile-image">
+                        <img src="https://assets.codepen.io/285131/neongirl.jpg" alt='imge-me'/>
+                        <h2>Gan Zhi Kiat</h2>
+                        <div className="social-media-icons">
+                            <a href="#x">
+                                <FontAwesomeIcon icon={faFacebook} size='25'/>
+                            </a>
+                            <a href="#x">
+                                <FontAwesomeIcon icon={faInstagram}/>
+                            </a>
+                            <a href="#x">
+                                <FontAwesomeIcon icon={faLinkedin}/>
+                            </a>
+                            <a href="#x">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                </div>
+                <div className="content-section">
+                    A student who graduated from TARUC, Bachelor's Degree in Information Technology (Internet Technology) with CGPA 3.63, made a dean list during my second year. 
+                    Activly joining lots of competitions and activities during university life. Doing an internship at CIMB Bank Malaysia, currently employed by 
+                    Hong Long Bank Malaysia as a Java software developer developing and maintaining the hong leong connect banking website for more than 3 years.
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Description;
